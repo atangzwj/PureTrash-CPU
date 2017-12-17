143318a5814f328c8003229e0ab1fe64424a2163
Arvin Tang <atangzwj@outlook.com>
2017-12-17 11:41:46 -0800

    muxes: Rewrote mux8_1 using gates Built mux8_1 out of gates to improve performance, critical path has been shortened

--- muxes/mux8_1.sv
+++ muxes/mux8_1.sv
@@ -6,11 +6,28 @@ module mux8_1 (
    input  logic [2:0] sel
 );
 
-   logic out0, out1;
-   mux4_1 m0 (.out(out0), .in(in[3:0]), .sel(sel[1:0]));
-   mux4_1 m1 (.out(out1), .in(in[7:4]), .sel(sel[1:0]));
+   parameter DELAY = 0.05;
 
-   mux2_1 mOut (.out, .in0(out0), .in1(out1), .sel(sel[2]));
+   logic [2:0] seln;
+   not #DELAY n0 (seln[0], sel[0]);
+   not #DELAY n1 (seln[1], sel[1]);
+   not #DELAY n2 (seln[2], sel[2]);
+
+   logic [7:0] mid0;
+   and #DELAY a0 (mid0[0], in[0], seln[2], seln[1], seln[0]);
+   and #DELAY a1 (mid0[1], in[1], seln[2], seln[1], sel[0]);
+   and #DELAY a2 (mid0[2], in[2], seln[2], sel[1],  seln[0]);
+   and #DELAY a3 (mid0[3], in[3], seln[2], sel[1],  sel[0]);
+   and #DELAY a4 (mid0[4], in[4], sel[2],  seln[1], seln[0]);
+   and #DELAY a5 (mid0[5], in[5], sel[2],  seln[1], sel[0]);
+   and #DELAY a6 (mid0[6], in[6], sel[2],  sel[1],  seln[0]);
+   and #DELAY a7 (mid0[7], in[7], sel[2],  sel[1],  sel[0]);
+
+   logic [1:0] mid1;
+   or #DELAY o0 (mid1[0], mid0[0], mid0[1], mid0[2], mid0[3]);
+   or #DELAY o1 (mid1[1], mid0[4], mid0[5], mid0[6], mid0[7]);
+
+   or #DELAY o2 (out, mid1[0], mid1[1]);
 endmodule
 
 module mux8_1_testbench ();
